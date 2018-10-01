df7753bc795285087812df22bda5cdf45fa3f548
arslan <arslan.mandzhiev.00@mail.ru>
2018-10-01 11:33:13 +0300

    сделал практически все

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -101,7 +101,8 @@ fun thirdDigit(number: Int): Int = number/100%10
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = TODO()
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int =
+        ((hoursArrive-hoursDepart)*60)+(minutesArrive-minutesDepart)
 
 /**
  * Простая
@@ -110,7 +111,10 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
+fun accountInThreeYears(initial: Int, percent: Int) : Double = (initial + ((initial+(initial*(percent/100.0)))*percent/100)
++ (initial + ((initial+(initial*(percent/100)))*(percent/100))*(percent/100)))
+
+
 
 /**
  * Простая
@@ -118,4 +122,4 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = TODO()
+fun numberRevert(number: Int): Int = (number%10)*100+((number%100)%10)*10 + number/100
